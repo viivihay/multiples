b800e30dae66802afb5b481a12c752ade2ab96bf
viivihay <viiivihay@gmail.com>
2025-03-16 18:38:03 +0200

    final touches

--- multiples.py
+++ multiples.py
@@ -8,9 +8,9 @@ Finally the script creates an output file containing sets of all the natural num
 
 def main(input_file, output_file):
     input_text = read_input_file(input_file)
-    rows = input_to_list(input_text, "\n")
+    rows = convert_str_to_list(input_text, "\n")
     output_list = create_output_list_with_multiples(rows)
-    formatted_output = form_output(output_list)
+    formatted_output = format_output(output_list)
     sorted_output = sort_output_lines(formatted_output)
     print(sorted_output)
     write_output_file(sorted_output, output_file)
@@ -21,7 +21,7 @@ def read_input_file(input_file):
         contents = f.read()
         return contents
     
-def input_to_list(input, separator=" "):
+def convert_str_to_list(input, separator=" "):
     """Parse given input string into a list using given separator and return the list."""
     input_list = input.split(separator)
     return input_list
@@ -30,14 +30,14 @@ def create_output_list_with_multiples(rows):
     """Loop through given rows and count the multiples of each row. Append results into an output list and return that."""
     output_list = []
     for row in rows:
-        row = input_to_list(row)
+        row = convert_str_to_list(row)
         output_list.append(f"{row[2]}:")
-        row_multiples_list = create_multiple_list(row[0], row[1], row[2])
+        row_multiples_list = create_list_of_multiples(row[0], row[1], row[2])
         row_multiples_list = remove_duplicates_and_sort(row_multiples_list)
         output_list.append(row_multiples_list)
     return output_list
 
-def create_multiple_list(A, B, C):
+def create_list_of_multiples(A, B, C):
     """Create and return a list of multiples using given parameters."""
     multiple_list = []
     multiple_list = count_multiples(A, C, multiple_list)
@@ -54,10 +54,10 @@ def count_multiples(multiple, limit, multiple_list):
     return multiple_list
 
 def remove_duplicates_and_sort(numbers):
-    """Convert list to set and back to list again to remove duplicates and sort the list ascending."""
+    """Convert list to set and back to list again to remove duplicates and sort the list in ascending order."""
     return list(set(numbers))
 
-def form_output(output_list):
+def format_output(output_list):
     """Form given output list into a string following spesific formatting style desired and return the outcome."""
     output_text = ""
     for i in output_list:
@@ -67,7 +67,7 @@ def form_output(output_list):
     return output_text
 
 def sort_output_lines(output):
-    """Sort given output ascending by the number of multiples found the line and return the sorted outcome."""
+    """Sort given output in ascending order by the number of multiples found the line and return the sorted outcome."""
     lines = output.split("\n")
     sorted_lines = sorted(lines, key=number_of_separators)
     sorted_output = "\n".join(sorted_lines)
