5835d7eba1eddb36ef010b57a8fff678a2145e76
viivihay <viiivihay@gmail.com>
2025-03-16 17:03:38 +0200

    better doc and readme, files as cmd args

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-# multiples
+# Multiples
+
+## Multiples of A and B
 
 A program that takes a file as the first command line argument. The file content has to be list of
 numbers, with 3 numbers per row separated by space and number of rows is undefined (can
 be something between 1 - infinite). First number in a row is A and the second is B, third one is
-the goal number (as in theorem 20). The program searches all multiples of A and B which
+the goal number. The program searches all multiples of A and B which
 are below the third number, prints them out to screen and also writes results to file which was given
 as the second command line argument.
 Program sorts out output file by ascending order how many multiples certain row has.
+
+---
+
+### Example usage:
+
+Input file (input.txt):
+
+> 1 8 31  
+> 4 7 20  
+> 3 9 15
+
+Command line command:  
+`python multiples.py "input.txt" "output.txt"`
+
+Command line output and output file:
+
+> 15:3, 6, 9, 12, 15  
+> 20:4, 7, 8, 12, 14, 16, 20  
+> 31:1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
--- multiples.py
+++ multiples.py
@@ -1,37 +1,19 @@
+import sys
+
 """Multiples of A and B
 
-This script allows the user to read an input file containing of A, B and C where C is the limit of multiples of A and B. 
-Finally the script creates an output file containing all the natural numbers below C that are multiples of A and B.
+This script allows the user to read an input file containing sets of A, B and C where C is the limit of multiples of A and B. 
+Finally the script creates an output file containing sets of all the natural numbers below C that are multiples of A and B.
 """
 
-def main():
-    input = read_input_file("input.txt")
-    output_list = []
-    case_list = input_to_list(input, "\n")
-    for case in case_list:
-        case = input_to_list(case)
-        output_list.append(f"{case[2]}:")
-        case_multiples_list = multiples(case[0], case[1], case[2])
-        output_list.append(case_multiples_list)
+def main(input_file, output_file):
+    input_text = read_input_file(input_file)
+    rows = input_to_list(input_text, "\n")
+    output_list = create_output_list_with_multiples(rows)
     formatted_output = form_output(output_list)
-    sorted_output = sort_output(formatted_output)
+    sorted_output = sort_output_lines(formatted_output)
     print(sorted_output)
-    write_output_file(sorted_output, "output.txt")
-
-def multiples(A, B, C):
-    factor_list = []
-    factor_list = count_multiples(A, C, factor_list)
-    factor_list = count_multiples(B, C, factor_list)
-    return factor_list
-
-def count_multiples(factor, limit, factor_list):
-    """Count the multiples with given limit and factor and add those to given list that is returned when done."""
-    factor = int(factor)
-    limit = int(limit)
-    multiple_times = limit // factor +1
-    for i in range(1, multiple_times):
-        factor_list.append(i*factor)
-    return factor_list
+    write_output_file(sorted_output, output_file)
 
 def read_input_file(input_file):
     """Read given input file contents and return it."""
@@ -44,18 +26,39 @@ def input_to_list(input, separator=" "):
     input_list = input.split(separator)
     return input_list
 
-def sort_output(output):
-    lines = output.split("\n")
-    sorted_lines = sorted(lines, key=number_of_separators)
-    sorted_output = "\n".join(sorted_lines)
-    return sorted_output
+def create_output_list_with_multiples(rows):
+    """Loop through given rows and count the multiples of each row. Append results into an output list and return that."""
+    output_list = []
+    for row in rows:
+        row = input_to_list(row)
+        output_list.append(f"{row[2]}:")
+        row_multiples_list = create_multiple_list(row[0], row[1], row[2])
+        row_multiples_list = remove_duplicates_and_sort(row_multiples_list)
+        output_list.append(row_multiples_list)
+    return output_list
 
-def number_of_separators(line):
-    parts = line.split(":")
-    numbers = parts[1].split(",") if len(parts) > 1 else []
-    return len(numbers)
+def create_multiple_list(A, B, C):
+    """Create and return a list of multiples using given parameters."""
+    multiple_list = []
+    multiple_list = count_multiples(A, C, multiple_list)
+    multiple_list = count_multiples(B, C, multiple_list)
+    return multiple_list
+
+def count_multiples(multiple, limit, multiple_list):
+    """Count the multiples with given limit and multiple and add those to given list that is returned when done."""
+    multiple = int(multiple)
+    limit = int(limit)
+    multiple_times = limit // multiple +1
+    for i in range(1, multiple_times):
+        multiple_list.append(i*multiple)
+    return multiple_list
+
+def remove_duplicates_and_sort(numbers):
+    """Convert list to set and back to list again to remove duplicates and sort the list ascending."""
+    return list(set(numbers))
 
 def form_output(output_list):
+    """Form given output list into a string following spesific formatting style desired and return the outcome."""
     output_text = ""
     for i in output_list:
         output_text += str(i)
@@ -63,9 +66,25 @@ def form_output(output_list):
         output_text = output_text.replace("[", "")
     return output_text
 
+def sort_output_lines(output):
+    """Sort given output ascending by the number of multiples found the line and return the sorted outcome."""
+    lines = output.split("\n")
+    sorted_lines = sorted(lines, key=number_of_separators)
+    sorted_output = "\n".join(sorted_lines)
+    return sorted_output
+
+def number_of_separators(line):
+    """Count and return the number of multiples on given line."""
+    parts = line.split(":")
+    numbers = parts[1].split(",") if len(parts) > 1 else []
+    return len(numbers)
+
 def write_output_file(output_text, output_file):
+    """Write given text into a file."""
     with open(output_file, "w") as f:
         f.write(output_text)
     
 if __name__ == "__main__":
-    main()
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    main(input_file, output_file)
